902bc80b60a5b6f3d8d7cd5cdca8f2d728d2a9d4
SigVad <97845386+SigVad@users.noreply.github.com>
2023-01-31 23:43:51 +0300

    роуты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,1 +1,42 @@
 // корневой компонент приложения, его создаёт CRA.
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import NotFound from '../NotFound/NotFound';
+
+function App() {
+  return (
+    <div className="page">
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/saved-movies">
+          <SavedMovies />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/signin">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Register />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+  
+export default App;
