f6cda6f6236bdb839c4fce4c45f277782aa0a02c
SigVad <97845386+SigVad@users.noreply.github.com>
2023-02-01 23:11:04 +0300

    навигация по О проекте

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,1 +1,14 @@
 // компонент с навигацией по странице «О проекте»
+import './NavTab.css';
+
+function NavTab() {
+ return (
+	<nav className="navtab">
+		<a href="#about-project" className='navtab__link'>О проекте</a>
+		<a href="#techs" className='navtab__link'>Технологии</a>
+		<a href="#about-me" className='navtab__link'>Студент</a>
+	</nav>
+ )
+}
+
+export default NavTab;
