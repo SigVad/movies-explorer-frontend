595b6450b7c1bbb7eb06319b9358d0e52709141d
SigVad <97845386+SigVad@users.noreply.github.com>
2023-02-05 16:29:51 +0300

    футер

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,17 @@
-// презентационный компонент, который отрисовывает подвал.
+// презентационный компонент, который отрисовывает подвал.import './Footer.css';
+import './Footer.css';
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<h3 className='footer__title footer__texts'>Учебный проект Яндекс.Практикум x BeatFilm.</h3>
+				<nav className='footer__nav'>
+				  <p className='footer__copyright footer__texts'>&copy; 2023</p>
+					<a href='https://practicum.yandex.ru/web/' className="footer__link footer__texts" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+					<a href='https://github.com/Dmitryzhur/' className="footer__link footer__texts" target="_blank" rel="noreferrer">Github</a>
+				</nav>
+		</footer>
+	)
+}
+
+export default Footer;
