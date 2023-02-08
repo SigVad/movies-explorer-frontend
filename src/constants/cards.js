4adffdf93218dcd53a1a0c58d4c54213039b56af
SigVad <97845386+SigVad@users.noreply.github.com>
2023-02-08 03:41:44 +0300

    предварительно карты

---
+++ src/constants/cards.js
@@ -0,0 +1,111 @@
+import pic_1 from '../images/content/pic__1.png';
+import pic_2 from '../images/content/pic__2.png';
+import pic_3 from '../images/content/pic__3.png';
+import pic_4 from '../images/content/pic__4.png';
+import pic_5 from '../images/content/pic__5.png';
+import pic_6 from '../images/content/pic__6.png';
+import pic_7 from '../images/content/pic__7.png';
+import pic_8 from '../images/content/pic__8.png';
+import pic_9 from '../images/content/pic__9.png';
+import pic_10 from '../images/content/pic__10.png';
+import pic_11 from '../images/content/pic__11.png';
+import pic_12 from '../images/content/pic__12.png';
+
+export const cardsList = [
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_1,
+    saved: true
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_2,
+    saved: true
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_3,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_4,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_3,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_5,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_6,
+    saved: true
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_2,
+    saved: true
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_3,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_4,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_7,
+    saved: true
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_8,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_9,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_10,
+    saved: false
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_11,
+    saved: true
+  },
+  {
+    name: 'В погоне за Бенкси',
+    duration: 27,
+    link: pic_12,
+    saved: false
+  }
+];
