ae400b011909499a4280f32bd5a5ee8a13ea362d
SigVad <97845386+SigVad@users.noreply.github.com>
2023-02-08 03:45:08 +0300

    список фильмов

---
+++ src/components/Movies-components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,31 @@
+// управляет отрисовкой карточек фильмов на страницу и их количеством.
+import React from "react";
+import "./MoviesCardList.css";
+import { cardsList } from "../../../constants/cards";
+import MoviesCard from "../MoviesCard/MoviesCard";
+
+function MoviesCardList({ location }) {
+
+  return (
+    <section className='cards__section'>
+      <ul className="cards__list">
+        { cardsList.map((card)=>{
+          return ( 
+              <MoviesCard
+                location = {location}
+                card={card}
+              />
+            )
+          }
+          )
+        }
+      </ul>
+      { (location === "/movies")
+          ? <button className='cards__button' type='button'>Ещё</button>
+          : <div className='cards__not-button'></div>
+      }
+       </section>
+  );
+}
+
+export default MoviesCardList;
