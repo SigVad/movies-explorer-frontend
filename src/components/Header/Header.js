77f332cf4a932eff3e2cc7d33ef5ff4d67e15809
SigVad <97845386+SigVad@users.noreply.github.com>
2023-02-08 03:39:21 +0300

    хедер

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,26 @@
 // компонент, который отрисовывает шапку сайта на страницу.
+
+import { Link } from "react-router-dom";
+import './Header.css';
+import logo from "../../images/logo.svg";
+import Navigation from '../Navigation/Navigation';
+
+function Header({ loggedIn, location="none" }) {
+  return (
+    <header className = {`header 
+    ${ (location === "/") && 'header_type_purple'}
+    ${ (location === "none") && 'header_type_none'}
+    }`}>
+      <Link className="header__link" to="/">
+        <img className="header__logo" src={logo} alt="логотип кольцо" />
+      </Link>
+
+      {
+        location !== "none" &&
+        <Navigation loggedIn={loggedIn} location = {location} />
+      }
+    </header>
+  )
+}
+
+export default Header;
