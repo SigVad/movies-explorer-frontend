a7baa8564034c2e930b96bf2192c2a0082498bb0
SigVad <97845386+SigVad@users.noreply.github.com>
2023-02-08 03:39:31 +0300

    логин

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,1 +1,55 @@
-// компонент страницы авторизации.
+// компонент страницы авторизации. /signin
+import '../Register/Register.css';
+import './Login.css';
+import Header from '../Header/Header';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  const loggedIn = false;
+
+  return (
+    <main className="register">
+      <Header loggedIn={loggedIn} />
+      <form action="/signin" className="form" noValidate>
+        <p className="form__title">Рады видеть!</p>
+        <label className="form__label">
+          <p className="form__text">E-mail</p>
+          <input
+            className="form__input"
+            name="email"
+            type="email"
+            id="email"
+            placeholder='Необходимо ввести e-mail'
+          />
+          <span className="form__input-error email__input-error">
+            Что-то пошло не так...
+          </span>
+        </label>
+        <label className="form__label">
+          <p className="form__text">Пароль</p>
+          <input
+            className="form__input"
+            type="password"
+            name="password"
+            id="pass"
+            minLength="7"
+          />
+          <span className="form__input-error pass__input-error">
+            Что-то пошло не так...
+          </span>
+        </label>
+        <button className="form__submit-button login__submit-button" type="submit">
+        Войти
+        </button>
+        <p className="form__text form__text_sub">
+        Еще не зарегистрированы?{" "}
+          <Link className="form__link" to="/signup">
+          Регистрация
+          </Link>
+        </p>
+      </form>
+    </main>
+  )
+}
+
+export default Login;
