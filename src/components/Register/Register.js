e85590156bb2d96dd8aafe269fc240c6557332ac
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-01 18:16:22 +0300

    форма регистрации

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,54 +1,87 @@
 // компонент страницы регистрации. /signup
+// мануал по React useForm
+// https://medium.com/nuances-of-programming/как-легко-создавать-формы-на-react-с-помощью-react-hook-form-9749c8cb3387
+
+
 import './Register.css';
 import Header from '../Header/Header';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
+import {useForm} from '../../hooks/useForm';
 
-function Register() {
-  const loggedIn = false;
+function Register(props) {
+
+  const authData =  useForm({name:'', email:'', password:''});
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onRegister({
+      name: authData.values.name,
+      email: authData.values.email,
+      password: authData.values.password,
+    });
+    authData.setValues({name:'', email:'', password:''});
+  }
 
   return (
     <main className="register">
-      <Header loggedIn={loggedIn} />
-      <form action="/signup" className="form" noValidate>
+      <Header loggedIn={props.loggedIn} />
+      <form 
+        action="/signup"
+        className="form"
+        noValidate
+        onSubmit={ handleSubmit }
+      >
         <p className="form__title">Добро пожаловать!</p>
         <p className="form__label">
-          <label for="name" className="form__text">Имя</label>
+          <label htmlFor="name" className="form__text">Имя</label>
           <input
+            required
+            minLength="2"
+            maxLength="30"
+            pattern="/^[A-Za-zА-яа-я]+$/i"
             className="form__input"
             name="name"
             id="name"
-            minLength="5"
-            maxLength="15"
-            placeholder='Необходимо ввести имя'
+            placeholder=''
+            onChange={authData.handleChange}
+            value={authData.values.name}
           />
           <span className="form__input-error name-input-error">
-          Что-то пошло не так...
           </span>
         </p>
         <p className="form__label">
-          <label for="email" className="form__text">E-mail</label>
+          <label htmlFor="email" className="form__text">E-mail</label>
           <input
+            required
+              // validate: (input) => isEmail(input), // true, если корректный
+            minLength="2" 
+            maxLength="30" 
             className="form__input"
             name="email"
             type="email"
             id="email"
-            placeholder='Необходимо ввести e-mail'
+            placeholder=''
+            onChange={authData.handleChange}
+            value={authData.values.email}
           />
           <span className="form__input-error">
-            Что-то пошло не так...
           </span>
         </p>
         <p className="form__label">
-          <label for="pass"  className="form__text">Пароль</label>
+          <label htmlFor="pass"  className="form__text">Пароль</label>
           <input
-            className="form__input form__input_type_error"
+            className="form__input"
             type="password"
             name="password"
             id="pass"
-            minLength="7"
+            placeholder=''
+            required
+            minLength="6" 
+            onChange={authData.handleChange}
+            value={authData.values.pass}
+            autoComplete="off"
           />
           <span className="form__input-error form__input-error_active">
-            Что-то пошло не так...
           </span>
         </p>
         <button className="form__submit-button" type="submit">
@@ -65,4 +98,4 @@ function Register() {
   )
 }
 
-export default Register;
+export default withRouter(Register);
