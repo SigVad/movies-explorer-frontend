5191d0948d6c6029d842111ff65dbc0e525b2de5
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-06 22:18:29 +0300

    показать фильмы из бд

--- src/components/Movies-components/SavedMovies/SavedMovies.js
+++ src/components/Movies-components/SavedMovies/SavedMovies.js
@@ -7,17 +7,47 @@ import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../../Footer/Footer';
 
-function SavedMovies() {
+function SavedMovies({
+  loggedIn,//статус авторизации, влияет на хедер
+  isLoading,//на прелоадер
+  shortFilmsDb,//чекбокс
+  toggleShortFilmsDb,//действие на чекбокс
+  onSearchDb,//Поиск
+  searchTextDb,//текст для фильтра
+  savedFilmsList,//фильмы из базы
+  quantityFilms, //выводимое количество фильмов
+  toggleSavedFilm,//если нажать на кнопку
+  savedFilms,//из бд
+  filmIsSaved,//проверка на наличие фильма в бд
+  onMoreFilmsClick,//если нажать на Ещё
+}) {
   const location = useLocation().pathname;
-  const loggedIn = true;
-    
+
   return (
     <>
       <Header loggedIn={loggedIn}  location = {location} />
       <main className='saved-movies'>
-        <SearchForm />
-        <Preloader />
-        <MoviesCardList location = {location} />
+        <SearchForm 
+          shortFilms = {shortFilmsDb}//чекбокс
+          toggleShortFilms = {toggleShortFilmsDb}//действие на чекбокс
+          onSearch = {onSearchDb}//Поиск
+          searchText = {searchTextDb}//текст для фильтра
+        />
+
+			  { (isLoading) 
+          ? <Preloader />
+          : (savedFilmsList === []) 
+            ? (<p className={`movies__message`}>Ничего не найдено</p>)
+            : <MoviesCardList
+                location = {location}
+                filmsList = {savedFilmsList}//фильмы movies из setFilms(поиск(короткие(localStorage)))
+                quantityFilms = {quantityFilms} //выводимое количество фильмов
+                toggleSavedFilm = {toggleSavedFilm}//если нажать на кнопку
+                savedFilms = {savedFilms}//из бд
+                filmIsSaved = {filmIsSaved}//проверка на наличие фильма в бд
+                onMoreFilmsClick = {onMoreFilmsClick}//если нажать на Ещё
+              />
+        }
       </main>
       <Footer />
     </>
