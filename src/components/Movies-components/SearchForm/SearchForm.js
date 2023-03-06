e487eb8f62a96af7a4e58f8456f4b192c0129a63
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-06 21:54:29 +0300

    форма Поиск

--- src/components/Movies-components/SearchForm/SearchForm.js
+++ src/components/Movies-components/SearchForm/SearchForm.js
@@ -1,16 +1,49 @@
 // форма поиска, куда пользователь будет вводить запрос.
 import './SearchForm.css';
+import { useState } from "react";
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
-function SearchForm({ onSubmit }) {
+function SearchForm({  
+    shortFilms, //чекбокс
+    toggleShortFilms, //действие на чекбокс
+    onSearch, //Поиск
+    searchText //текст для фильтра
+  }){
+	const [text, setText] = useState(searchText);
+  
+
+  function onSubmit(evt){
+		evt.preventDefault();
+    onSearch(text);
+  }
+
+	function handleSearch(evt) {
+		setText(evt.target.value)
+	}
+
   return (
     <div className='search-form'>
-      <form className='search-form__container' name='SearchMovie' onSubmit={onSubmit}>
-        <input className='search-form__input' type='text' placeholder='Фильм' name='search-movie' required></input>
+      <form 
+        className='search-form__container' 
+        name='SearchMovie' 
+        onSubmit={onSubmit}
+      >
+        <input 
+          className='search-form__input' 
+          type='text' 
+          placeholder='Фильм' 
+          name='search-movie' 
+          value={text}
+          // required
+          onChange={handleSearch}
+        ></input>
         <button className='search-form__button' type='submit'>Поиск</button>
       </form>
       <div className='search-form__checkbox'>
-        <FilterCheckbox />
+        <FilterCheckbox 
+          shortFilms={shortFilms}
+          toggleShortFilms={toggleShortFilms}
+        />
       </div>
     </div>
   )
