1e7b5dad72d14a1c12b224922d986aa35cbc9118
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-06 21:49:38 +0300

    чекбокс

--- src/components/Movies-components/FilterCheckbox/FilterCheckbox.js
+++ src/components/Movies-components/FilterCheckbox/FilterCheckbox.js
@@ -1,21 +1,16 @@
 // фильтр с чекбоксом «Только короткометражки»
 import './FilterCheckbox.css';
-import { useCallback, useState, memo } from "react";
 
-const FilterCheckbox = memo(() => {
-	const [isSelected, setIsSelected] = useState(false);
-	const onSelect = useCallback(
-	  () => setIsSelected(!isSelected),
-	  [isSelected]
-	);
-	return (
-		<div className="checkbox">
-			<div className={`checkbox__button ${isSelected ? "checkbox__button_is-selected" : ""}`} onClick={onSelect}>
-              <div className={`checkbox__button-circle ${isSelected ? "checkbox__button-circle_is-selected" : ""}`} />
-			</div>
-			<div className="checkbox__title">Короткометражки</div>
-		</div>
-	);
-});
+function FilterCheckbox({ shortFilms, toggleShortFilms }) {
+  // console.log(`FilterCheckbox ${shortFilms}`);
+  return (
+    <div className="checkbox">
+      <div className={`checkbox__button ${shortFilms ? "" : "checkbox__button_is-selected"}`} onClick={toggleShortFilms}>
+          <div className={`checkbox__button-circle ${shortFilms ? "" : "checkbox__button-circle_is-selected"}`} />
+      </div>
+      <div className="checkbox__title">Короткометражки</div>
+    </div>
+  );
+};
 
 export default FilterCheckbox;
