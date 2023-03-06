432c6650f35a7370ad53b3c38f7cfa2281a486d5
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-06 21:52:10 +0300

    константы

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+// const MAIN_DB_URL = 'https://api.sigvad.nomoredomains.rocks'
+const MAIN_DB_URL = 'http://127.0.0.1:3000'
+const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+const IMAGES_URL = 'https://api.nomoreparties.co';
+
+const SHORT_DURATION = 40;
+
+export { MAIN_DB_URL, IMAGES_URL, MOVIES_URL, SHORT_DURATION };
