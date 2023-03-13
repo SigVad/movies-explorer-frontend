ea93a9cb8c1bd41ae8091b9c36372be0d6462e56
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-14 02:40:22 +0300

    вынес функционал

--- src/components/Movies-components/MoviesCard/MoviesCard.js
+++ src/components/Movies-components/MoviesCard/MoviesCard.js
@@ -1,6 +1,5 @@
 // компонент одной карточки фильма.
 import "./MoviesCard.css";
-import { useCallback, useState } from "react";
 import iconX from "../../../images/icon-X.svg";
 import iconV from "../../../images/icon-V.svg";
 
@@ -8,46 +7,41 @@ import iconV from "../../../images/icon-V.svg";
 function MoviesCard({ 
   card,
   location,
-  filmIsSaved,
+  isSaved,
   toggleSavedFilm
 }) {
-
-	const [isSelected, setIsSelected] = useState(filmIsSaved(card.movieId));
-	
-  const onSelect = useCallback(
-	  () => {
-      setIsSelected(!isSelected);
-      toggleSavedFilm(card);
-    },
-	  [isSelected]
-	);
   
   function isMoviesPage() {
-      if (location === "/movies") {
-        return true; // iconV
-      }
-      return false; // iconX
+    if (location === "/movies") {
+      return true; // iconV
     }
-
-    return (
+    return false; // iconX
+  }
+  
+  return (
     <li className="card">
       <div className="card__container">
         <h2 className="card__title">{card.nameRU}</h2>
         <p className="card__duration">{card.duration} минут</p>
       </div>
-			<a className="card__link" href={card.trailerLink} target="_blank" rel="noreferrer">
+      <a className="card__link" href={card.trailerLink} target="_blank" rel="noreferrer">
         <img className="card__photo" src={card.image} alt="постер фильма" />
-			</a>
+      </a>
         {
-          isSelected
+          isSaved
             ? <button type="button" className={`card__button 
-                ${isMoviesPage() ? "card__button_red" : ""
-              }`} onClick={onSelect}>
+                ${isMoviesPage() ? "card__button_red" : ""}`} 
+                onClick={
+                  ()=>{toggleSavedFilm(card, isSaved)}
+              }>
                 <img className="card__button-icon" 
                   src={isMoviesPage() ? iconV : iconX
                 } alt="иконка" />
               </button>
-            : <button type="button" className="card__button" onClick={onSelect}>
+            : <button type="button" className="card__button" 
+                onClick={
+                  ()=>{toggleSavedFilm(card, isSaved)}
+              }>
                 Сохранить
               </button>
         }
