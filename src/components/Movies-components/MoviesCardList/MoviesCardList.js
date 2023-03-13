ef39f46d66106259aa8c1ff8c2493a5fa2e918c1
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-14 02:41:55 +0300

    добавил обработку сохраненных

--- src/components/Movies-components/MoviesCardList/MoviesCardList.js
+++ src/components/Movies-components/MoviesCardList/MoviesCardList.js
@@ -2,38 +2,56 @@
 import React from "react";
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
+import { useState, useEffect } from "react";
 
 function MoviesCardList({ 
   location,
   filmsList, //фильмы movies из setFilms(поиск(короткие(localStorage)))
   quantityFilms, //выводимое количество фильмов
   toggleSavedFilm, //если нажать на кнопку
-  savedFilms, //из бд
-  filmIsSaved, //проверка на наличие фильма в бд
+  inspectFilmIsSaved, //проверка на наличие фильма в бд
   onMoreFilmsClick, //если нажать на Ещё
 }) {
-  console.log(`${filmsList.length} > ${quantityFilms}`);
+  
+  // console.log(`${filmsList.length} > ${quantityFilms}`);
   return (
     
     <section className='cards'>
       <ul className="cards__list">
         { 
         filmsList.map((card, index)=>{
-          if (index < quantityFilms) {
+          if (index < quantityFilms && location === "/movies"){
+            let save = false;
+            let id = inspectFilmIsSaved(card.movieId);
+            if (id) {
+              card._id = id;
+              save = true;
+            }
             return ( 
               <MoviesCard
                 location = {location}
                 key={card.movieId}
                 card={card}
-                filmIsSaved={filmIsSaved}
+                isSaved={save}
                 toggleSavedFilm={toggleSavedFilm}
               />
             )
-          } 
+          } else if (location === "/saved-movies"){
+            return ( 
+              <MoviesCard
+                location = {location}
+                key={card.movieId}
+                card={card}
+                isSaved={true}
+                toggleSavedFilm={toggleSavedFilm}
+              />
+            )
+          }
+
         })
         }
       </ul>
-      { (filmsList.length > quantityFilms)
+      { (filmsList.length > quantityFilms && location === "/movies")
           ? <button className='cards__button' type='button' onClick={onMoreFilmsClick}>Ещё</button>
           : <div className='cards__not-button'></div>
       }
