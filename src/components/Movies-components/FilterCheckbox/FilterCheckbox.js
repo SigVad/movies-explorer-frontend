4ef9121448f7fa2b8fc3cca5fe0d6bb874a51194
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-14 02:36:25 +0300

    изменен вызов финкции

--- src/components/Movies-components/FilterCheckbox/FilterCheckbox.js
+++ src/components/Movies-components/FilterCheckbox/FilterCheckbox.js
@@ -1,11 +1,17 @@
 // фильтр с чекбоксом «Только короткометражки»
 import './FilterCheckbox.css';
 
-function FilterCheckbox({ shortFilms, toggleShortFilms }) {
+function FilterCheckbox({ location, shortFilms, toggleShortFilms }) {
+  
   // console.log(`FilterCheckbox ${shortFilms}`);
+  function onClick(evt){
+		evt.preventDefault();
+    toggleShortFilms();
+  }
+
   return (
     <div className="checkbox">
-      <div className={`checkbox__button ${shortFilms ? "" : "checkbox__button_is-selected"}`} onClick={toggleShortFilms}>
+      <div className={`checkbox__button ${shortFilms ? "" : "checkbox__button_is-selected"}`} onClick={onClick}>
           <div className={`checkbox__button-circle ${shortFilms ? "" : "checkbox__button-circle_is-selected"}`} />
       </div>
       <div className="checkbox__title">Короткометражки</div>
