13ce6b2e7e32d4bd7141fd494a71beae84d10170
SigVad <97845386+SigVad@users.noreply.github.com>
2023-03-26 17:34:31 +0300

    перейти на апи продакшн

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// const MAIN_DB_URL = 'https://api.sigvad.nomoredomains.rocks'
-const MAIN_DB_URL = 'http://127.0.0.1:3000'
+const MAIN_DB_URL = 'https://api.sigvad.nomoredomains.rocks'
+// const MAIN_DB_URL = 'http://127.0.0.1:3000'
 const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 const IMAGES_URL = 'https://api.nomoreparties.co';
 
